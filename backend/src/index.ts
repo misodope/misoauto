3887ef14ce89c618cd5f3a732a10cf0c2956df86
Jerry Chen <misodope@cap-rx.com>
2023-03-27 19:36:00 -0400

    setup graphql express server

---
+++ backend/src/index.ts
@@ -0,0 +1,46 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import gql from "graphql-tag";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello, world!",
+  },
+};
+
+const app = express();
+const httpServer = http.createServer(app);
+const { json } = bodyParser;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  "/graphql",
+  cors<cors.CorsRequest>(),
+  json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve)
+);
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
