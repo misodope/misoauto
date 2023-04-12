437b297002de4c3c85ac227e77c101577f9614bf
Jerry Chen <misodope@cap-rx.com>
2023-04-11 21:23:58 -0400

    update footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,10 +1,11 @@
 const Footer = () => (
-  <footer className="bg-gray-100 py-4">
-    <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-      <p className="text-gray-600 text-sm">
+  <footer className="bg-gray-100 py-4 sm:py-8">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-around">
+      <p className="text-gray-600 text-sm sm:mb-0 mb-4">
         &copy; 2023 MisoAuto. All rights reserved.
       </p>
-      <div>
+      <div className="flex flex-wrap flex-col">
+        <p className="text-gray-600 text-sm sm:mb-0 font-bold">Legal</p>
         <a
           href="/privacy"
           className="text-gray-600 text-sm hover:text-gray-800 mr-4"
