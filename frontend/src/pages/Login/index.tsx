4efd6ebbb8a0aada38fcf5d0dfa0a5fd37d03447
Jerry Chen <misodope@cap-rx.com>
2023-04-13 22:37:57 -0400

    switch to a tag

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,29 +1,29 @@
 export const Login = () => {
-  const handleTikTokLogin = async () => {
-    try {
-      const response = await fetch(
-        "https://misoauto.up.railway.app/oauth/tiktok",
-        {
-          method: "GET",
-          mode: "no-cors",
-        }
-      );
+  // const handleTikTokLogin = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://misoauto.up.railway.app/oauth/tiktok",
+  //       {
+  //         method: "GET",
+  //         mode: "no-cors",
+  //       }
+  //     );
 
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <div className="container mx-auto flex flex-col items-center">
       <h1 className="text-3xl font-bold  mb-10">Login</h1>
-      <div
+      <a
         className="cursor-pointer bg-black text-white py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
-        onClick={() => handleTikTokLogin()}
+        href="https://misoauto.up.railway.app/oauth/tiktok"
       >
         Continue with TikTok
-      </div>
+      </a>
     </div>
   );
 };
