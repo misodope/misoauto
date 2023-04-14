f5c84c3b41d9234b45bd0d00a8f25c5a54a99b0b
Jerry Chen <misodope@cap-rx.com>
2023-04-14 00:43:09 -0400

    Add prisma generate to docker

--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -3,9 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Connect the client
   await prisma.$connect();
-  // ... you will write your Prisma Client queries here
+
   const allUsers = await prisma.user.findMany();
   console.log(allUsers);
 }
