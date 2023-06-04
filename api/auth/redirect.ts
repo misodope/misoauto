7b9a6c307a7e2c35b035e8b635153f90bb537ddb
Jerry Chen <misodope@cap-rx.com>
2023-06-04 11:00:31 -0400

    refactor queries

--- api/auth/redirect.ts
+++ api/auth/redirect.ts
@@ -3,8 +3,7 @@ import {
   TikTokSuccessResponse,
 } from "../../services/auth/AuthController.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import prisma from "../../services/prisma/index.js";
-import { add } from "date-fns";
+import UserQueries from "@services/prisma/queries/user.js";
 
 const handler = async (req: VercelRequest, res: VercelResponse) => {
   const { code, state } = req.query as { code: string; state: string };
@@ -16,46 +15,17 @@ const handler = async (req: VercelRequest, res: VercelResponse) => {
 
   try {
     const authController = new AuthController();
+    const userQueries = new UserQueries();
     const response: TikTokSuccessResponse = await authController.getAccessToken(
       code
     );
-    console.log("response", response);
-    // use prisma and check if user already exists in db otherwise create new user
-    await prisma.$connect();
-    console.log("AFTER CONNECT", prisma);
 
-    let user = await prisma.user.findUnique({
-      where: { openId: response.open_id },
-    });
-
-    console.log("HELLLOOO", user);
-    await prisma.$disconnect();
-    // if (user) {
-    //   user = await prisma.user.update({
-    //     where: { openId: response.open_id },
-    //     data: {
-    //       accessToken: response.access_token,
-    //       refreshToken: response.refresh_token,
-    //       expiresIn: response.expires_in,
-    //       scope: response.scope,
-    //       refreshExpiresIn: response.refresh_expires_in,
-    //       expiresAt: add(new Date(), { seconds: response.expires_in }),
-    //       refreshExpiresAt: add(new Date(), { seconds: response.expires_in }),
-    //     },
-    //   });
-    // } else {
-    //   user = await prisma.user.create({
-    //     data: {
-    //       openId: response.open_id,
-    //       accessToken: response.access_token,
-    //       refreshToken: response.refresh_token,
-    //       expiresIn: response.expires_in,
-    //       scope: response.scope,
-    //       refreshExpiresIn: response.refresh_expires_in,
-    //     },
-    //   });
-    // }
-    // console.log("USER", user);
+    const user = await userQueries.getUser(response.open_id);
+    if (!user) {
+      await userQueries.createUser(response);
+    } else {
+      await userQueries.updateUser(response.open_id, response);
+    }
   } catch (error) {
     console.error(error);
   }
---
+++ services/prisma/queries/user.ts
@@ -0,0 +1,59 @@
+import { TikTokSuccessResponse } from "@services/auth/AuthController.js";
+import prisma from "../index.js";
+import { add } from "date-fns";
+
+export default class UserQueries {
+  async getUser(openId: string) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { openId },
+      });
+
+      return user;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async createUser(data: TikTokSuccessResponse) {
+    try {
+      const user = await prisma.user.create({
+        data: {
+          openId: data.open_id,
+          accessToken: data.access_token,
+          refreshToken: data.refresh_token,
+          expiresIn: data.expires_in,
+          scope: data.scope,
+          refreshExpiresIn: data.refresh_expires_in,
+          expiresAt: add(new Date(), { seconds: data.expires_in }),
+          refreshExpiresAt: add(new Date(), { seconds: data.expires_in }),
+        },
+      });
+
+      return user;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async updateUser(openId: string, data: TikTokSuccessResponse) {
+    try {
+      const user = await prisma.user.update({
+        where: { openId },
+        data: {
+          accessToken: data.access_token,
+          refreshToken: data.refresh_token,
+          expiresIn: data.expires_in,
+          scope: data.scope,
+          refreshExpiresIn: data.refresh_expires_in,
+          expiresAt: add(new Date(), { seconds: data.expires_in }),
+          refreshExpiresAt: add(new Date(), { seconds: data.expires_in }),
+        },
+      });
+
+      return user;
+    } catch (error) {
+      return error;
+    }
+  }
+}
