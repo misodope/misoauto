8f97381a53b9784a4647c4866bbe166c282bd82e
Jerry Chen <misodope@cap-rx.com>
2023-09-05 19:02:09 -0400

    try stuff

--- api/auth/redirect.ts
+++ api/auth/redirect.ts
@@ -10,7 +10,7 @@ import {
   APIGatewayProxyResult,
 } from "aws-lambda";
 
-import { PrismaClient, User } from "@prisma/client/aws";
+import { PrismaClient, User } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
