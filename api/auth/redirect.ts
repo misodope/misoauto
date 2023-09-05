8531fc44e466b6cf88bce92109fcc55bbbce6c9d
Jerry Chen <misodope@cap-rx.com>
2023-09-05 18:25:20 -0400

    redirect

--- api/auth/redirect.ts
+++ api/auth/redirect.ts
@@ -3,7 +3,6 @@ import {
   TikTokSuccessResponse,
 } from "../../services/api/AuthController.js";
 import UserQueries from "../../services/prisma/queries/user.js";
-import { getRedirectUrl } from "../../services/utils/env.js";
 import {
   Context,
   APIGatewayProxyEventV2,
@@ -21,11 +20,11 @@ export const handler: Handler = async (
 ): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-
-  const { code, state } = event.queryStringParameters as {
-    code: string;
-    state: string;
-  };
+  console.log("PRISMA", prisma);
+  // const { code, state } = event.queryStringParameters as {
+  //   code: string;
+  //   state: string;
+  // };
 
   // const { csrfState } = event.cookies;
   // if (state !== csrfState) {
@@ -33,43 +32,49 @@ export const handler: Handler = async (
   //   return;
   // }
 
-  try {
-    const authController = new AuthController();
-    const userQueries = new UserQueries(prisma);
-    const redirectUri =
-      "https://ilywoklih4.execute-api.us-east-1.amazonaws.com/api/auth/redirect/";
+  // try {
+  //   const authController = new AuthController();
+  //   const userQueries = new UserQueries(prisma);
+  //   const redirectUri =
+  //     "https://ilywoklih4.execute-api.us-east-1.amazonaws.com/api/auth/redirect/";
 
-    const response: TikTokSuccessResponse = await authController.getAccessToken(
-      code,
-      redirectUri,
-    );
+  //   const response: TikTokSuccessResponse = await authController.getAccessToken(
+  //     code,
+  //     redirectUri,
+  //   );
 
-    let user: User = await userQueries.getUser(response.open_id);
+  //   let user: User = await userQueries.getUser(response.open_id);
 
-    if (!user) {
-      user = await userQueries.createUser(response);
-    } else {
-      user = await userQueries.updateUser(response.open_id, response);
-    }
+  //   if (!user) {
+  //     user = await userQueries.createUser(response);
+  //   } else {
+  //     user = await userQueries.updateUser(response.open_id, response);
+  //   }
 
-    const handlerResponse: APIGatewayProxyResult = {
-      statusCode: 302,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Credentials": true,
-        Location: `https://dl7rsqqwy6kne.cloudfront.net/dashboard/?user=${user.openId}`,
-      },
-      body: JSON.stringify({ message: "Redirecting to dashboard" }),
-    };
+  //   const handlerResponse: APIGatewayProxyResult = {
+  //     statusCode: 302,
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Access-Control-Allow-Headers": "*",
+  //       "Access-Control-Allow-Credentials": true,
+  //       Location: `https://dl7rsqqwy6kne.cloudfront.net/dashboard/?user=${user.openId}`,
+  //     },
+  //     body: JSON.stringify({ message: "Redirecting to dashboard" }),
+  //   };
 
-    return handlerResponse;
-  } catch (error) {
-    console.error(error);
-    const handlerErrorResponse: APIGatewayProxyResult = {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
-    };
-    return handlerErrorResponse;
-  }
+  //   return handlerResponse;
+  // } catch (error) {
+  //   console.error(error);
+  //   const handlerErrorResponse: APIGatewayProxyResult = {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ message: "Internal Server Error" }),
+  //   };
+  //   return handlerErrorResponse;
+  // }
+  const handlerResponse: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Refresh Endpoint!" }),
+  };
+
+  return handlerResponse;
 };
