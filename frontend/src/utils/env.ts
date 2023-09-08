894a7fd2fe8056053080644bd7a2803638fdc4ab
Jerry Chen <misodope@cap-rx.com>
2023-09-07 21:08:43 -0400

    hardcode

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,8 +1,5 @@
 export const getApiUrl = () => {
   console.log("META ENV", import.meta.env);
   console.log("PROCESS ENV", process.env);
-  return (
-    import.meta.env.VITE_API_URL ||
-    "https://ilywoklih4.execute-api.us-east-1.amazonaws.com/api"
-  );
+  return "https://ilywoklih4.execute-api.us-east-1.amazonaws.com/api";
 };
