62072e1dbc1b6f6d55ff3290ece8240f5647286f
Jerry Chen <misodope@cap-rx.com>
2023-09-07 21:27:07 -0400

    update

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,5 +1,4 @@
 export const getApiUrl = () => {
   console.log("META ENV", import.meta.env);
-  console.log("PROCESS ENV", process.env);
   return "https://ilywoklih4.execute-api.us-east-1.amazonaws.com/api";
 };
