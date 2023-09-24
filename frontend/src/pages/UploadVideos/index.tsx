d957a22538ed19ec81fecd080588881223a25b73
Jerry Chen <misodope@cap-rx.com>
2023-09-24 12:38:06 -0400

    add upload video page and file upload input

--- frontend/src/pages/UploadVideos/index.tsx
+++ frontend/src/pages/UploadVideos/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { PageContainer } from "../../components/PageContainer/PageContainer";
 import { PageTitle } from "../../components/PageTitle/PageTitle";
 
 export const UploadVideos = (): React.ReactElement => {
+  const [uploadFile, setUploadFile] = useState<null | unknown>(null);
+  const handleChooseFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUploadFile(e.target.value);
+  };
+  console.log("Selected File", uploadFile);
   return (
     <PageContainer>
       <PageTitle>Upload Videos</PageTitle>
+      <div className="w-96 h-40 border-dashed rounded border-2 flex items-center justify-center">
+        <input type="file" onChange={handleChooseFile} />
+      </div>
     </PageContainer>
   );
 };
