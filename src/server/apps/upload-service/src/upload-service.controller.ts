e19fb39ec7713bb0801a759ae4e0bfd9c0aaa376
Jerry Chen <jerry@curbwaste.com>
2024-03-19 19:38:35 -0400

    Add docker

--- src/server/apps/upload-service/src/upload-service.controller.ts
+++ src/server/apps/upload-service/src/upload-service.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { UploadServiceService } from './upload-service.service';
 
-@Controller()
+@Controller('api')
 export class UploadServiceController {
   constructor(private readonly uploadServiceService: UploadServiceService) {}
 
-  @Get()
+  @Get('upload')
   getHello(): string {
     return this.uploadServiceService.getHello();
   }
