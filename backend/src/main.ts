ce628ed21c8576a0a1705caf6b6b84361a56945e
Jerry Chen <jerry@misodope.com>
2026-04-05 15:15:26 -0500

    ok now it works

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ import { Logger } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common';
 
 async function bootstrap() {
-  console.log('wtf is going on here');
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
